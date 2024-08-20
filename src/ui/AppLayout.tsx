1d76a2e2990b4cadde40e181b8471d53e9a820c6
icarodredd <icarog854@gmail.com>
2024-08-20 14:15:29 -0300

    add error and loading generics

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -1,11 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
+import Loader from "./Loader";
 
 export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
+  console.log(navigation);
   return (
     <div>
       <Header />
+      {isLoading && <Loader />}
 
       <main>
         <h1>Content</h1>
